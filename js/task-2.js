c2532835f9e4ebfe9ac1c320aff71faaf0636403
Stanislav Chumak <dp300792csv@gmail.com>
2025-11-11 22:11:07 -0800

    task2

---
+++ js/task-2.js
@@ -0,0 +1,59 @@
+//! solution 1
+class Storage {
+  #items;
+
+  constructor(array) {
+    this.#items = array;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
+}
+
+//! solution 2
+// class Storage {
+//   #items;
+
+//   constructor(array) {
+//     this.#items = array;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     const updatedArray = [];
+//     this.#items.forEach((item) => {
+//       if (item !== itemToRemove) {
+//         updatedArray.push(item);
+//       }
+//     });
+//     this.#items = updatedArray;
+//   }
+// }
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
